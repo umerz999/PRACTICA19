19b8e896d0fcc6750f021b377b20205b69de9dc1
umerz999 <57722214+umerz999@users.noreply.github.com>
2019-12-11 15:13:30 +0100

    Add files via upload

---
+++ show-attackers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+readonly LIMIT='10'
+LOG_FILE="${1}"
+
+#Asegurarse que se puesto un fichero de parametro correcto
+if [[ ! -e "${LOG_FILE}" ]]
+then
+  echo "El fichero no existe en este directorio o no has puesto ningún fichero: ${LOG_FILE}" >&2
+  exit 1
+fi
+
+# Mostrar Información
+echo 'Count,IP,Location'
+
+# Bucle listando y buscando por IPs.
+grep Failed syslog-sample | grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" \
+| sort | uniq -c | while read COUNT IP; do
+  # Si el número de intentos fallidos es mayor que LIMIT
+  if [ "${COUNT}" -gt "${LIMIT}" ]; then
+      LOCATION=$(geoiplookup "${IP}" | awk -F ', ' '{print $2}')
+      echo "${COUNT},${IP},${LOCATION}"
+  fi
+done
+
+
+exit 0
